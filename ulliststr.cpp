afdfbbf2385bfddafa4a8f1c6937acec3d9b2f7f
Jake Hosking <hosking@usc.edu>
2022-09-06 01:05:41 -0700

    Half of ulliststr.cpp coded

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,71 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val) {
+  if (ULListStr::empty()) {
+    head_ = tail_ = new Item;
+
+    tail_->first = 4;
+    tail_->last = 5;
+    tail_->val[4] = val;
+  }
+  else if(tail_->last != 10) {
+    tail_->val[tail_->last++] = val;
+  }
+  else {
+    Item* temp = tail_;
+    tail_ = new Item;
+    temp->next = tail_;
+    tail_->prev = temp;
+
+    tail_->first = 0;
+    tail_->last = 1;
+    tail_->val[0] = val;
+  }
+
+  size_++;
+}
+
+void ULListStr::push_front(const std::string& val) {
+  if (ULListStr::empty()) {
+    head_ = tail_ = new Item;
+
+    head_->first = 4;
+    head_->last = 5;
+    head_->val[4] = val;
+  }
+  else if(head_->first != 0) {
+    head_->val[--head_->first] = val;
+  }
+  else {
+    Item* temp = head_;
+    head_ = new Item;
+    temp->prev = head_;
+    head_->next = temp;
+
+    tail_->first = 9;
+    tail_->last = 10;
+    tail_->val[9] = val;
+  }
+
+  size_++;
+}
+
+void ULListStr::pop_back() {
+  if(ULListStr::empty()) {
+    return;
+  }
+  else if(tail_->last != 1) {
+    tail_->last--;
+  }
+  else {
+    Item* temp;
+    temp = tail_->prev;
+    delete tail_;
+    tail_ = temp;
+    tail_->next = nullptr;
+  }
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
